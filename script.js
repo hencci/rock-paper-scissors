bd953e2eddaecd10c8395d625d0cd03d226fe8fb
hencci <hencci20@gmail.com>
2024-11-06 15:39:17 +0100

    Write the logic to randomly return Rock, Paper, or Scissors

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getComputerChoice() {
+    let cpuChoice;
+    let choice = Math.floor(Math.random() * 3) + 1;
     
-}
+    if (choice === 1) {
+        cpuChoice = "ROCK";
+    }
+    else if (choice === 2) {
+        cpuChoice = "PAPER";
+    }
+    else {
+        cpuChoice = "SCISSORS";
+    }
 
+    return cpuChoice;
+}
+console.log(getComputerChoice());
