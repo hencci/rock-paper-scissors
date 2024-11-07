ec71f712801da2d537ad081c2a1773c324f3f102
hencci <hencci20@gmail.com>
2024-11-07 16:52:34 +0100

    Move score variables and playRound function into playGame function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let cpuScore = 0;
-
 function getComputerChoice() {
     let cpuChoice;
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -32,34 +29,35 @@ function getHumanChoice() {
     
 }
 
-function playRound(humanChoice, cpuChoice) {
-    let message = console.log(`Your choice: ${humanChoice} | CPU choice: ${cpuChoice}`);
-    
-    if (humanChoice === cpuChoice) {
-        message;
-        console.log("It is a tie")
-    }
-    else if (humanChoice === "ROCK" && cpuChoice === "PAPER" || 
-    humanChoice === "PAPER" && cpuChoice === "SCISSORS" || 
-    humanChoice === "SCISSORS" && cpuChoice === "ROCK") {
-        message;
-        console.log(`You lose! ${cpuChoice} defeats ${humanChoice}`);
-        cpuScore += 1;
-    }
-    else if (humanChoice === "ROCK" && cpuChoice === "SCISSORS" || 
-    humanChoice === "PAPER" && cpuChoice === "ROCK" || 
-    humanChoice === "SCISSORS" && cpuChoice === "PAPER") {
-        message;
-        console.log(`You Win! ${humanChoice} defeats ${cpuChoice}`);
-        humanScore += 1;
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
 function playGame() {
-    
+    let humanScore = 0;
+    let cpuScore = 0;
+
+    function playRound(humanChoice, cpuChoice) {
+        let message = console.log(`Your choice: ${humanChoice} | CPU choice: ${cpuChoice}`);
+        
+        if (humanChoice === cpuChoice) {
+            message;
+            console.log("It is a tie")
+        }
+        else if (humanChoice === "ROCK" && cpuChoice === "PAPER" || 
+        humanChoice === "PAPER" && cpuChoice === "SCISSORS" || 
+        humanChoice === "SCISSORS" && cpuChoice === "ROCK") {
+            message;
+            console.log(`You lose! ${cpuChoice} defeats ${humanChoice}`);
+            cpuScore += 1;
+        }
+        else if (humanChoice === "ROCK" && cpuChoice === "SCISSORS" || 
+        humanChoice === "PAPER" && cpuChoice === "ROCK" || 
+        humanChoice === "SCISSORS" && cpuChoice === "PAPER") {
+            message;
+            console.log(`You Win! ${humanChoice} defeats ${cpuChoice}`);
+            humanScore += 1;
+        }
+    }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
 }
