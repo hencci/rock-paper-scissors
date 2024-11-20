1acea1ea758600dabb689a020d87d359cc040839
hencci <hencci20@gmail.com>
2024-11-20 10:30:54 +0100

    Rewrite getCompterChoice function to return lower case values

--- script.js
+++ script.js
@@ -25,20 +25,18 @@ scissors.addEventListener("click", (e) => {
 })
 
 function getComputerChoice() {
-    let cpuChoice;
+
     let choice = Math.floor(Math.random() * 3) + 1;
     
     if (choice === 1) {
-        cpuChoice = "ROCK";
+        return "rock";
     }
     else if (choice === 2) {
-        cpuChoice = "PAPER";
+        return "paper";
     }
     else {
-        cpuChoice = "SCISSORS";
+        return "scissors";
     }
-
-    return cpuChoice;
 }
 
 function playGame() {
